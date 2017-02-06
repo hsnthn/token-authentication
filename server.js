d0d7f50ab32198434e7eac0af4d925c63bc23a78
hsnthn <hasantahan90@gmail.com>
2017-02-06 15:11:57 +0300

    init

--- server.js
+++ server.js
@@ -1,40 +1,28 @@
-// =================================================================
-// get the packages we need ========================================
-// =================================================================
 var express 	= require('express');
 var app         = express();
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
 
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
-var config = require('./config'); // get our config file
-var User   = require('./app/models/user'); // get our mongoose model
+var jwt    = require('jsonwebtoken'); 
+var config = require('./config'); 
+var User   = require('./app/models/user'); 
 
-// =================================================================
-// configuration ===================================================
-// =================================================================
-var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
-mongoose.connect(config.database); // connect to database
-app.set('superSecret', config.secret); // secret variable
+var port = process.env.PORT || 8080;
+mongoose.connect(config.database); 
+app.set('superSecret', config.secret);
 
-// use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// use morgan to log requests to the console
 app.use(morgan('dev'));
 
-// =================================================================
-// routes ==========================================================
-// =================================================================
+// create manuel user
 app.get('/setup', function(req, res) {
 
-	// create a sample user
 	var nick = new User({ 
 		name: 'Hasan Tahan', 
-		password: 'elpsycongroo',
-		admin: true 
+		password: 'elpsycongroo'
 	});
 	nick.save(function(err) {
 		if (err) throw err;
@@ -42,11 +30,9 @@ app.get('/setup', function(req, res) {
 		console.log('User saved successfully');
 		res.json({ success: true });
 	});
-// create a sample user
         var second_nick = new User({
                 name: 'Mehmet Ali Gozaydin',
-                password: 'elpsycongroo',
-                admin: true
+                password: 'elpsycongroo'
         });
         second_nick.save(function(err) {
                 if (err) throw err;
@@ -57,23 +43,14 @@ app.get('/setup', function(req, res) {
 
 });
 
-// basic route (http://localhost:8080)
 app.get('/', function(req, res) {
-	res.send('Hello! The API is at http://localhost:' + port + '/api');
+	res.send('The API is  http://localhost:' + port + '/api');
 });
 
-// ---------------------------------------------------------
-// get an instance of the router for api routes
-// ---------------------------------------------------------
 var apiRoutes = express.Router(); 
 
-// ---------------------------------------------------------
-// authentication (no middleware necessary since this isnt authenticated)
-// ---------------------------------------------------------
-// http://localhost:8080/api/authenticate
 apiRoutes.post('/authenticate', function(req, res) {
 
-	// find the user
 	User.findOne({
 		name: req.body.name
 	}, function(err, user) {
@@ -84,7 +61,6 @@ apiRoutes.post('/authenticate', function(req, res) {
 			res.json({ success: false, message: 'Authentication failed. User not found.' });
 		} else if (user) {
 
-			// check if password matches
 			if (user.password != req.body.password) {
 				res.json({ success: false, message: 'Authentication failed. Wrong password.' });
 			} else {
@@ -107,23 +83,16 @@ apiRoutes.post('/authenticate', function(req, res) {
 	});
 });
 
-// ---------------------------------------------------------
-// route middleware to authenticate and check token
-// ---------------------------------------------------------
 apiRoutes.use(function(req, res, next) {
 
-	// check header or url parameters or post parameters for token
 	var token = req.body.token || req.param('token') || req.headers['x-access-token'];
 
-	// decode token
 	if (token) {
 
-		// verifies secret and checks exp
 		jwt.verify(token, app.get('superSecret'), function(err, decoded) {			
 			if (err) {
 				return res.json({ success: false, message: 'Failed to authenticate token.' });		
 			} else {
-				// if everything is good, save to request for use in other routes
 				req.decoded = decoded;	
 				next();
 			}
@@ -142,11 +111,8 @@ apiRoutes.use(function(req, res, next) {
 	
 });
 
-// ---------------------------------------------------------
-// authenticated routes
-// ---------------------------------------------------------
 apiRoutes.get('/', function(req, res) {
-	res.json({ message: 'Welcome to the coolest API on earth!' });
+	res.json({ message: 'Welcome' });
 });
 
 apiRoutes.get('/users', function(req, res) {
@@ -161,8 +127,4 @@ apiRoutes.get('/check', function(req, res) {
 
 app.use('/api', apiRoutes);
 
-// =================================================================
-// start the server ================================================
-// =================================================================
 app.listen(port);
-console.log('Magic happens at http://localhost:' + port);
